c757389d03cc80a5bd25c59ba49ee4393a38b8ae
Ahmadfhk327 <ahmadfhk327@gmail.com>
2025-01-18 01:16:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
+console.log(data);
+app.use(cors()); // Allowing access to all clients
+// Simple GET endpoint
+app.get("/api", (req, res) => {
+  res.json({
+    status: "success",
+    data: data,
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`API is running on http://localhost:${port}`);
+});
